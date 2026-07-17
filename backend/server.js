53cb556393756a95a7a5c247a278ef14806ddaf6
urtuqa6013-web <urtuqa6013@gmail.com>
2026-07-17 16:10:45 +0500

    add cors config

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,22 @@ import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://task-manager-urwa.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
-app.use(cors());
 app.use(morgan("dev"));
 
 app.use("/api/v1/auth", authRoutes);
