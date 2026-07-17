0dd0d43a16065f6c4177d2673ee292ccb0666f7d
urtuqa6013-web <urtuqa6013@gmail.com>
2026-07-17 16:42:03 +0500

    save

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: "https://backend.skymail.beer/",
+  baseURL: "https://backend.skymail.beer/api/v1",
   withCredentials: true,
 });
 
